f5cbbbe9cb3d1c98f2dbf130ccb05e2c9508cc0a
Võ Nhật Duy <86996188+duynhatvo05@users.noreply.github.com>
2025-11-16 00:57:41 +0700

    update cart

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -674,7 +674,7 @@ function Cart() {
                         fill="white"
                       />
                     </svg>
-                    Chuyển khoản QR (PayOS)
+                    QR Bank Transfer (PayOS)
                   </label>
                 </div>
               </section>
